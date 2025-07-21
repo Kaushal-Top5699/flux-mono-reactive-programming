fff46045e3db5681aece27f20de2ea309761e5d9
Kaushal Topinkatti B <kaushaltop@me.com>
2025-07-21 18:14:43 -0500

    Operators filter() and flatMap() practice.

--- src/main/java/reactive/programming/FluxAndMono.java
+++ src/main/java/reactive/programming/FluxAndMono.java
@@ -19,6 +19,18 @@ public class FluxAndMono {
         System.out.println("\n");
         System.out.println("Reactive Streams are immutable, the elements are not converted to upper case.");
         immutableReactiveStreamsExample().subscribe(System.out::println);
+        System.out.println("\n");
+        System.out.println("Flux with filter() operator:");
+        namesFluxFilter().subscribe(System.out::println);
+
+        System.out.println("\n");
+        System.out.println("Assignment 2:");
+        namesMono_map_filter(5).subscribe(System.out::println);
+
+        System.out.println("\n");
+        System.out.println("Operator flatMap() example: ");
+        flatMapExample().subscribe(System.out::println);
+
     }
 
 
@@ -43,5 +55,24 @@ public class FluxAndMono {
         var names = Flux.fromIterable(List.of("Kevin", "Cloe", "Andy", "Marcus", "Amanda", "Joe", "Alexander"));
         names.map(String::toUpperCase);
         return names;
+    }
+
+    // Flux with filter() operator.
+    public static Flux<String> namesFluxFilter() {
+        return Flux.fromIterable(List.of("Kevin", "Cloe", "Andy", "Marcus", "Amanda", "Joe", "Alexander"))
+                .filter(name -> name.length() > 6);
+    }
+
+    // Mono with map() and filter() operators.
+    public static Mono<String> namesMono_map_filter(int stringLength) {
+        return Mono.just("alex")
+                .map(String::toUpperCase)
+                .filter(name -> name.length() >= stringLength);
+    }
+
+    // Flux with flatMap() operator.
+    public static Flux<String> flatMapExample() {
+        return Flux.fromIterable(List.of("Kevin", "Cloe", "Andy", "Marcus", "Amanda", "Joe", "Alexander"))
+                .flatMap(name -> Flux.fromArray(name.split("")));
     }
 }
--- src/test/java/services/FluxAndMonoServiceTest.java
+++ src/test/java/services/FluxAndMonoServiceTest.java
@@ -2,6 +2,7 @@ package services;
 
 import org.junit.jupiter.api.Test;
 import reactive.programming.FluxAndMono;
+import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 public class FluxAndMonoServiceTest {
@@ -27,6 +28,22 @@ public class FluxAndMonoServiceTest {
         var names = FluxAndMono.namesFluxMap();
         StepVerifier.create(names)
                 .expectNext("KEVIN", "CLOE", "ANDY", "MARCUS", "AMANDA", "JOE", "ALEXANDER")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFluxFilter() {
+        var names = FluxAndMono.namesFluxFilter();
+        StepVerifier.create(names)
+                .expectNext("Alexander")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesMono_map_filter() {
+        var name = FluxAndMono.namesMono_map_filter(2);
+        StepVerifier.create(name)
+                .expectNext("ALEX")
                 .verifyComplete();
     }
 }
