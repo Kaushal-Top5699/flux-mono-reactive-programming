8c26ae0166f9caf74191267bf12b6d6a148e19ca
Kaushal Topinkatti B <kaushaltop@me.com>
2025-07-22 14:16:42 -0500

    flatMap() and flatMapMany() with Mono.

--- src/main/java/reactive/programming/FluxAndMono.java
+++ src/main/java/reactive/programming/FluxAndMono.java
@@ -4,7 +4,9 @@ import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.Random;
 
 public class FluxAndMono {
     public static void main(String[] args) {
@@ -30,6 +32,18 @@ public class FluxAndMono {
         System.out.println("\n");
         System.out.println("Operator flatMap() example: ");
         flatMapExample().subscribe(System.out::println);
+
+//        System.out.println("\n");
+//        System.out.println("Asynchronous nature of flatmap() operator:");
+//        flatMapAsync().subscribe(System.out::println);
+//
+//        System.out.println("\n");
+//        System.out.println("Flux with concatMap() operator:");
+//        concatMapExample().subscribe(System.out::println);
+
+        System.out.println("\n");
+        System.out.println("Mono with flatMapMany() operator: ");
+        flatMapMany().subscribe(System.out::println);
 
     }
 
@@ -74,5 +88,54 @@ public class FluxAndMono {
     public static Flux<String> flatMapExample() {
         return Flux.fromIterable(List.of("Kevin", "Cloe", "Andy", "Marcus", "Amanda", "Joe", "Alexander"))
                 .flatMap(name -> Flux.fromArray(name.split("")));
+    }
+
+    // Async nature of flatMap() operator. Part-1.
+    private static Flux<String> splitStringWithDelay(String name) {
+        var charArray = name.split("");
+        var delayRandom = new Random().nextInt(1000);
+        var delay = 1000;
+        System.out.println("Current delay: "+delay);
+        return Flux.fromArray(charArray)
+                .delayElements(Duration.ofMillis(delay));
+    }
+
+    // Async nature of flatMap() operator. Part-2.
+    public static Flux<String> flatMapAsync() {
+        return Flux.fromIterable(List.of("Kevin", "Cloe", "Andy", "Marcus", "Amanda", "Joe", "Alexander"))
+                .map(String::toUpperCase)
+                .flatMap(FluxAndMono::splitStringWithDelay).log();
+    }
+
+    // Flux concatMap() operator - similar to flatMap() but it preserves the order. Part-1.
+    public static Flux<String> concatMapExample() {
+        return Flux.fromIterable(List.of("Kevin", "Cloe", "Andy", "Marcus", "Amanda", "Joe", "Alexander"))
+                .concatMap(name -> Flux.fromArray(name.split("")));
+    }
+
+    // Flux concatMap() operator - similar to flatMap() but it preserves the order. Part-2.
+    public static Flux<String> concatMapExampleWithDelay() {
+        return Flux.fromIterable(List.of("Kevin", "Cloe", "Andy", "Marcus", "Amanda", "Joe", "Alexander"))
+                .concatMap(FluxAndMono::splitStringWithDelay).log();
+    }
+
+    // The flatMap() operator with Mono.
+    public Mono<List<String>> namesMono_flatMap(int stringLength) {
+        return Mono.just("alex")
+                .map(String::toUpperCase)
+                .filter(name -> name.length() >= stringLength)
+                .flatMap(this::splitStringMono);
+    }
+
+    private Mono<List<String>> splitStringMono(String s) {
+        var charArray = s.split("");
+        var charList = List.of(charArray);
+        return Mono.just(charList);
+    }
+
+    // flatMapMany() operator with Mono - Converts one thing to many things.
+    public static Flux<String> flatMapMany() {
+        Mono<String> nameMono = Mono.just("Kevin");
+        return nameMono.flatMapMany(name -> Flux.fromArray(name.split("")));
     }
 }
--- src/test/java/services/FluxAndMonoServiceTest.java
+++ src/test/java/services/FluxAndMonoServiceTest.java
@@ -5,6 +5,8 @@ import reactive.programming.FluxAndMono;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.util.List;
+
 public class FluxAndMonoServiceTest {
 
     @Test
@@ -44,6 +46,44 @@ public class FluxAndMonoServiceTest {
         var name = FluxAndMono.namesMono_map_filter(2);
         StepVerifier.create(name)
                 .expectNext("ALEX")
+                .verifyComplete();
+    }
+
+    @Test
+    void flatMapExample() {
+        var names = FluxAndMono.flatMapExample();
+        StepVerifier.create(names)
+                .expectNext("K",
+                        "e", "v", "i", "n", "C", "l", "o", "e", "A", "n", "d", "y", "M", "a", "r", "c", "u", "s",
+                        "A", "m", "a", "n", "d", "a", "J", "o", "e", "A", "l", "e", "x", "a", "n", "d", "e", "r")
+                .verifyComplete();
+    }
+
+    @Test
+    void flatMapAsync() {
+        var names = FluxAndMono.flatMapAsync();
+        StepVerifier.create(names)
+                .expectNextCount(37)
+                .verifyComplete();
+    }
+
+    @Test
+    void concatMapExampleWithDelay() {
+        var names = FluxAndMono.concatMapExampleWithDelay();
+        StepVerifier.create(names)
+                .expectNext("K",
+                        "e", "v", "i", "n", "C", "l", "o", "e", "A", "n", "d", "y", "M", "a", "r", "c", "u", "s",
+                        "A", "m", "a", "n", "d", "a", "J", "o", "e", "A", "l", "e", "x", "a", "n", "d", "e", "r")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesMono_flatMap() {
+        FluxAndMono fluxAndMono = new FluxAndMono();
+        int strLen = 3;
+        var name = fluxAndMono.namesMono_flatMap(strLen);
+        StepVerifier.create(name)
+                .expectNext(List.of("A", "L", "E", "X"))
                 .verifyComplete();
     }
 }
